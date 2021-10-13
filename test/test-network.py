7c7eba7eaa9cd03cc2cc7f35d1978cdda9e44378
Tommy Guess <70559469+togu1895@users.noreply.github.com>
2021-10-13 11:06:35 -0600

    Update test-network.py

--- test/test-network.py
+++ test/test-network.py
@@ -1,60 +1,54 @@
-import base64
-import re
+#import base64
+#import re
 import sys
-import hmac
-import hashlib
-import itertools
-import json
+#import hmac
+#import hashlib
+#import itertools
 import os
 import os.path
 import subprocess
-import zipfile
+#import zipfile
 import sys
-import signal
+#import signal
 import time
-import urllib2
+import urllib.request
 
 
-def grade_network(file):
-  path = "./"
-  network_grades = []
+path = "./"
+network_grades = []
+
+
+
+try:
+  # Launch python file
+  #p = os.spawnl(os.P_NOWAIT, '/usr/bin/python', 'python', python_file)
+  subprocess.run(['python3', 'attack.py'])
+    
+  time.sleep(1)
+  # Make webrequest
+  response = urllib.request.urlopen('http://freeaeskey.xyz/')
+  #response = urllib.urlretrieve('http://freeaeskey.xyz/')
   
-  p1 = os.path.normpath(os.path.join(path, file))
-  print('Grading "%s"' % p1)
-  
-  if os.path.exists(p1):
-  
-    try:
-      # Launch python file
-      p = os.spawnl(os.P_NOWAIT, '/usr/bin/python', 'python', python_file)
-      time.sleep(1)
-      # Make webrequest
-      response = urllib2.urlopen('http://freeaeskey.xyz/')
-      resp = response.read()
+  resp = response.read()
 
-      if '4d6167696320576f7264733a2053717565616d697368204f7373696672616765' in resp:
-        printc(colors.GREEN, 'Key injected!')
-        print(resp)
-        os.kill(p, signal.SIGKILL)
-        network_grades.append((10,''))
-      else:
-        printc(colors.RED, 'failed to inject key')
-        os.kill(p, signal.SIGKILL)
-        network_grades.append(0,("Failed to inject key."))
-
-      #results = subprocess.check_output(['python', python_file], env=self.new_env)
-    except KeyboardInterrupt:
-      print('Hit Ctrl-C, stopping')
-      sys.exit(1)
-    except Exception as e:
-      printc(colors.RED, 'hmm: %s' % e)
-      network_grades.append((0,"Code failure."))
+  if '4d6167696320576f7264733a2053717565616d697368204f7373696672616765' in resp:
+    print('Key injected!')
+    print(resp)
+    #os.kill(p, signal.SIGKILL)
+    network_grades.append((10,''))
   else:
-    network_grades.append((0,"No file submitted."))
+    print('failed to inject key')
+    #os.kill(p, signal.SIGKILL)
+    network_grades.append(0,("Failed to inject key."))
+
+  #results = subprocess.check_output(['python', python_file], env=self.new_env)
+except Exception as e:
+  print("Failure - python exception.")
+  network_grades.append((0,"Code failure."))
+
+#else:
+#  network_grades.append((0,"No file submitted."))
+#  sys.exit(-1)
   
-  print(network_grades)
-  sys.exit(0)
-  
-  
-if __name__ == '__main__':
-  grade_network(sys.argv[1])
+print(network_grades)
+sys.exit(0)
